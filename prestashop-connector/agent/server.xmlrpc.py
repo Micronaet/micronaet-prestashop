008d312c4b9719fce45a47818fd6261339cc9f50
nicothebrush <nicola.riolini@gmail.com>
2017-03-17 15:00:18 +0100

    fast git

--- prestashop-connector/agent/server.xmlrpc.py
+++ prestashop-connector/agent/server.xmlrpc.py
@@ -133,8 +133,9 @@ def execute(model, operation, *parameter, **parameter_dict):
         
         # ------------------
         # 2. Create product:
-        # ------------------
+        # ------------------        
         elif operation == 'create':
+            import pdb; pdb.set_trace()
             return mysql_db.create(parameter, parameter_dict)
         
         # ------------------
